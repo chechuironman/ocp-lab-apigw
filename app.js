85a2c5feb267eaaaa60acf63b111c4937a0a4a65
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-02-01 20:11:21 +0100

    secret fix

--- app.js
+++ app.js
@@ -13,17 +13,16 @@ function main () {
   var cors=require('cors');
 
 // app.use(cors({origin:true,credentials: true}));
-app.options('*', cors());
+  app.options('*', cors());
   app.use(bodyParser.urlencoded({ // Middleware
     extended: true
   }));
-  app.use(function(req,res,next){
+
+  app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-});
+  });
   app.use(bodyParser.json());
   app.use("/api/users", users);
   app.use("/api/backend", backend);
