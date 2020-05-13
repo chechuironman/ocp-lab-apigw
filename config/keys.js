8d5e8ca8e652d24b138cb34dbcb567b93f7435d0
Chechu <chechu@Chechus-MacBook-Pro-2.local>
2020-05-13 13:11:29 +0200

    test

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     login_service: "http://host.docker.internal:3001/api/users/login",
-    create_user_service: "http://host.docker.internal:8081/create_user",
+    create_user_service: "http://host.docker.internal:8080/create_user",
     register_user_service: "http://localhost:3001/api/users/register",
     list_course_management_service: "http://host.docker.internal:3002/api/management/list_courses",
     register_course_service: "http://host.docker.internal:3002/api/management/register_course",
